1973ed5e2addbe511de15d3789d29def0f8a776f
JordaneMaurelli <jordane.maurelli@gmail.com>
2024-01-05 16:14:27 -0300

    Criation structure of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Header from './components/Header'
+import Article from './components/Article'
+import Post from './components/Post'
 
 function App() {
   return (
-    <h1>Meu blog</h1>
+    <>
+      <Header></Header>
+      <Article></Article>
+      <Post></Post>
+      <h1>Meu blog</h1>
+    </>
   );
 }
 
